f2b29d1dc3798f4acdecaeac547e2ece690174bd
Evgenii <eugene.suvorikov@gmail.com>
2022-11-03 22:53:55 +0300

    jar with executable bat converter to exe

---
+++ Engine/src/SungearEngine2D/exception/SungearEngineException.java
@@ -0,0 +1,15 @@
+package SungearEngine2D.exception;
+
+import java.io.IOException;
+
+public class SungearEngineException extends Exception {
+
+
+    public SungearEngineException(SungearEngineError error, String... arguments) {
+        super(new IOException(String.format(error.getErrorText(), arguments)));
+    }
+
+    public SungearEngineException(Throwable throwable, SungearEngineError error, String... arguments) {
+        super(new IOException(String.format(error.getErrorText(), arguments), throwable));
+    }
+}
