f0ba54caf0136580048f444790cb6965c0333800
pfhgil <99049610+pfhgil@users.noreply.github.com>
2023-02-04 00:35:58 +0300

    Fixed PP

--- Core2D/src/main/java/Core2D/Deserializers/LayeringDeserializer.java
+++ Core2D/src/main/java/Core2D/Deserializers/LayeringDeserializer.java
@@ -1,7 +1,10 @@
 package Core2D.Deserializers;
 
+import Core2D.ECS.Component.Components.Camera2DComponent;
+import Core2D.ECS.Entity;
 import Core2D.Layering.Layer;
 import Core2D.Layering.Layering;
+import Core2D.Layering.PostprocessingLayer;
 import com.google.gson.*;
 
 import java.lang.reflect.Type;
@@ -20,6 +23,19 @@ public class LayeringDeserializer implements JsonDeserializer<Layering>
         for(JsonElement element : layers) {
             Layer layer = context.deserialize(element, Layer.class);
             layering.getLayers().add(layer);
+        }
+
+        for(Layer layer : layering.getLayers()) {
+            for(Entity entity : layer.getEntities()) {
+                for (Camera2DComponent camera2DComponent : entity.getAllComponents(Camera2DComponent.class)) {
+                    for (int i = 0; i < camera2DComponent.getPostprocessingLayersNum(); i++) {
+                        PostprocessingLayer ppLayer = camera2DComponent.getPostprocessingLayer(i);
+
+                        ppLayer.setEntitiesLayerToRender(layering.getLayer(ppLayer.getEntitiesLayerToRenderName()));
+                        //ppLayer.entitiesLayerToRender = layer;
+                    }
+                }
+            }
         }
         return layering;
     }
